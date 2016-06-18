7a79e49f615469eab9dc6c106e018db2917fb603
spam4kev <spam4kev@gmail.com>
2016-06-18 23:03:59 +0000

    add security group checks

--- onyd.sh
+++ onyd.sh
@@ -4,11 +4,17 @@ message="Oh No You Di'int. Shutting off so you can redeploy with corrected value
 declare -A CHECK
 #build key/value pairs where key is meta-data url endpoint to check if it returns a good value
 #                   desired domain                desired key
-CHECK=( [hostname]=".ec2.internal" [public-keys]="fitz-pass" [security-groups]="ssh-from-home")
+CHECK=( [hostname]=".ec2.internal" [public-keys]="fitz-pass" [security-groups]="ssh-from-home","test" )
 
 for I in "${!CHECK[@]}"
 do
-    curl -s $url/$I/ | grep -q ${CHECK[$I]} ||  echo "Error: $I of value ${CHECK[$I]} not permitted. $message" #&& sudo poweroff
+    for J in $(echo "${CHECK[$I]}" | sed "s/,/ /g")
+    do
+        echo "      Error: $I of value $J not permitted." &&\
+        echo "             $message" &&\
+        logger "Error: $I of value $J not permitted." $message #&& sudo poweroff
+    done
+    #curl -s $url/$I/ | grep -q ${CHECK[$I]} ||  echo "Error: $I of value ${CHECK[$I]} not permitted. $message" #&& sudo poweroff
     #curl -s $url/$I/ | grep -q ${CHECK[$I]} && echo "Error: $I of value ${CHECK[$I]} not permitted. $message" #&& sudo poweroff
 done
 #curl -s http://169.254.169.254/1.0/meta-data/hostname/ | grep -q ec2.internal && echo "shutting down"
