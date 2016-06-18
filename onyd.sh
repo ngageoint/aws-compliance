2e217ee4663b1b10dcedd9c15e270f474a0424e9
spam4kev <spam4kev@gmail.com>
2016-06-18 21:32:04 +0000

    wip

--- onyd.sh
+++ onyd.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 url="http://169.254.169.254/1.0/meta-data"
-message="Oh no you di'int. Shutting off so you can redeploy with corrected value using procedures found here: <wiki link here>"
+message="Oh No You Di'int. Shutting off so you can redeploy with corrected value using procedures found here: <wiki link here>"
 declare -A CHECK
 #build key/value pairs where key is meta-data url endpoint to check if it returns a naughty value
 CHECK=( [hostname]=".ec2.internal" )
