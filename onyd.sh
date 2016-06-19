264092eac3c67e9c8b5bc7f0e56f8c3da82b1943
spam4kev <spam4kev@gmail.com>
2016-06-19 18:37:18 +0000

    version 1 code implemented but needs testing

--- onyd.sh
+++ onyd.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+# This script enforces orginizational AWS instance standards enabling ease of VPC security accreditation.
+# To implement, create a root users cron referencing this script in your Org's Linux AMI like below.
+# root cront content: @reboot /var/lib/cloud/scripts/per-boot/onyd.sh
+#
+# 6/19/16: KF
 url="http://169.254.169.254/1.0/meta-data"
 message="Oh No You Di'int. Shutting off so you can redeploy with corrected value using procedures found here: <wiki link here>"
 declare -A CHECK
@@ -8,7 +13,7 @@ CHECK=( [hostname]=".ec2.internal" [public-keys]="fitz-pass" [security-groups]="
 
 function shutheroff {
    logger "      Error: $I of value \"$J\" not found on this instance. $message"
-   #sudo poweroff
+   sudo poweroff
 }
 function main {
 for I in "${!CHECK[@]}"
@@ -17,10 +22,8 @@ do
     do
         curl -s $url/$I/ | grep -q $J || shutheroff
     done
-    #curl -s $url/$I/ | grep -q ${CHECK[$I]} ||  echo "Error: $I of value ${CHECK[$I]} not permitted. $message" #&& sudo poweroff
-    #curl -s $url/$I/ | grep -q ${CHECK[$I]} && echo "Error: $I of value ${CHECK[$I]} not permitted. $message" #&& sudo poweroff
+    #curl -s http://169.254.169.254/1.0/meta-data/hostname/ | grep -q ec2.internal || sudo poweroff"
 done
-#curl -s http://169.254.169.254/1.0/meta-data/hostname/ | grep -q ec2.internal && echo "shutting down"
 }
 
 main
