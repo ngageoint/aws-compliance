2f7e1342a91b5335a734398b35fcb6e9a8261b17
spam4kev <spam4kev@gmail.com>
2016-06-20 00:25:06 +0000

    corrected

--- onyd.sh
+++ onyd.sh
@@ -8,7 +8,7 @@
 #
 url="http://169.254.169.254/1.0/meta-data"
 message="Oh No You Di'int! :) Shutting off so you can terminate me (and my volumes) and redeploy with corrected values using procedures found here: <wiki link here>"
-email="spam4kev@gmail.com"
+email="spam4kev@gmail.com" #update this to your email address
 declare -A CHECK
 #build key/value pairs where key is meta-data url endpoint to check if it returns a good value
 #                   desired domain                desired key                  comma seperated list of sg's
@@ -16,12 +16,12 @@ CHECK=( [hostname]=".ec2.internal" [public-keys]="fitz-pass" [security-groups]="
 
 function notify {
    logger "      Error: $I of value \"$J\" not found on this instance. $message"
-   echo "      Error: $I of value \"$J\" not found on this instance. $message" | mail -s "Powering off your instance: $(hostname)" $email
+   echo "      Error: onyd.sh mailer - $I of value \"$J\" not found on this instance. $message" | mail -s "Powering off your instance: $(hostname)" $email
 }
 
 function shutheroff {
    notify
-   [[ "$1" == "noop" ]] && poweroff
+   [[ "$1" != "noop" ]] && poweroff
 }
 function main {
 for I in "${!CHECK[@]}"
